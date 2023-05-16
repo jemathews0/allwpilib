4b3860405d626b0adfaebfd4cfc57ffe4b47e3e9
Jonathan Mathews <jonathanemathews@gmail.com>
2023-05-15 19:37:20 -0600

    Modify simulated CAN to use Phoenix SocketCAN

--- hal/src/main/native/sim/CAN.cpp
+++ hal/src/main/native/sim/CAN.cpp
@@ -5,18 +5,24 @@
 #include "hal/CAN.h"
 
 #include "mockdata/CanDataInternal.h"
+#include "ctre/phoenix/platform/Platform.h"
+#include <cstring>
 
 using namespace hal;
 
+const char* CAN_BUS = "can0";
+
 namespace hal::init {
-void InitializeCAN() {}
+void InitializeCAN() {
+  //ctre::phoenix::platform::can::RegisterCANbus(CAN_BUS);
+}
 }  // namespace hal::init
 
 extern "C" {
 
 void HAL_CAN_SendMessage(uint32_t messageID, const uint8_t* data,
                          uint8_t dataSize, int32_t periodMs, int32_t* status) {
-  SimCanData->sendMessage(messageID, data, dataSize, periodMs, status);
+  ctre::phoenix::platform::can::CANComm_SendMessage(messageID, data, dataSize, periodMs, status, CAN_BUS);
 }
 void HAL_CAN_ReceiveMessage(uint32_t* messageID, uint32_t messageIDMask,
                             uint8_t* data, uint8_t* dataSize,
@@ -25,8 +31,8 @@ void HAL_CAN_ReceiveMessage(uint32_t* messageID, uint32_t messageIDMask,
   // handler
   *dataSize = 42;
   auto tmpStatus = *status;
-  SimCanData->receiveMessage(messageID, messageIDMask, data, dataSize,
-                             timeStamp, status);
+  ctre::phoenix::platform::can::CANComm_ReceiveMessage(messageID, messageIDMask, data, dataSize,
+                             timeStamp, status, CAN_BUS);
   // If no handler invoked, return message not found
   if (*dataSize == 42 && *status == tmpStatus) {
     *status = HAL_ERR_CANSessionMux_MessageNotFound;
@@ -35,24 +41,34 @@ void HAL_CAN_ReceiveMessage(uint32_t* messageID, uint32_t messageIDMask,
 void HAL_CAN_OpenStreamSession(uint32_t* sessionHandle, uint32_t messageID,
                                uint32_t messageIDMask, uint32_t maxMessages,
                                int32_t* status) {
-  SimCanData->openStreamSession(sessionHandle, messageID, messageIDMask,
-                                maxMessages, status);
+  ctre::phoenix::platform::can::CANComm_OpenStreamSession(sessionHandle, messageID, messageIDMask,
+                                maxMessages, status, CAN_BUS);
 }
 void HAL_CAN_CloseStreamSession(uint32_t sessionHandle) {
-  SimCanData->closeStreamSession(sessionHandle);
+  ctre::phoenix::platform::can::CANComm_CloseStreamSession(sessionHandle, CAN_BUS);
 }
 void HAL_CAN_ReadStreamSession(uint32_t sessionHandle,
                                struct HAL_CANStreamMessage* messages,
                                uint32_t messagesToRead, uint32_t* messagesRead,
                                int32_t* status) {
-  SimCanData->readStreamSession(sessionHandle, messages, messagesToRead,
-                                messagesRead, status);
+  static struct HAL_CANStreamMessage messages_buf[128];
+  static ctre::phoenix::platform::can::canframe_t canframe_messages[128];
+  ctre::phoenix::platform::can::CANComm_ReadStreamSession(sessionHandle, canframe_messages, 
+      messagesToRead, messagesRead, status, CAN_BUS);
+  for(size_t i=0; i < *messagesRead; i++)
+  {
+    messages_buf[i].messageID = canframe_messages[i].arbID;
+    messages_buf[i].timeStamp = canframe_messages[i].timeStampUs;
+    int toCopy = canframe_messages[i].len > 8 ? 8 : canframe_messages[i].len;
+    memcpy(messages_buf[i].data, canframe_messages[i].data, toCopy);
+    messages_buf[i].dataSize = toCopy;
+  }
 }
 void HAL_CAN_GetCANStatus(float* percentBusUtilization, uint32_t* busOffCount,
                           uint32_t* txFullCount, uint32_t* receiveErrorCount,
                           uint32_t* transmitErrorCount, int32_t* status) {
-  SimCanData->getCANStatus(percentBusUtilization, busOffCount, txFullCount,
-                           receiveErrorCount, transmitErrorCount, status);
+  ctre::phoenix::platform::can::CANbus_GetStatus(*percentBusUtilization, *busOffCount, *txFullCount,
+                           *receiveErrorCount, *transmitErrorCount, *status, CAN_BUS);
 }
 
 }  // extern "C"
